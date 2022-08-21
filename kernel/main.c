df2e8ac61d8ef0fbbec42cea89d3487a96f7d797
leiwenyong <aleiwenyong@outlook.com>
2022-08-21 16:38:10 +0800

    加入中断

--- kernel/main.c
+++ kernel/main.c
@@ -1,7 +1,10 @@
 #include "lib.h"
 #include "printk.h"
+#include "gate.h"
+#include "trap.h"
 
-void Start_Kernel(void){
+void Start_Kernel(void)
+{
 	int *addr = (int *)0xffff800000a00000;
 	int i;
 
@@ -17,7 +20,49 @@ void Start_Kernel(void){
 	Pos.FB_addr = (int *)0xffff800000a00000;
 	Pos.FB_length = (Pos.XResolution * Pos.YResolution * 4);
 
-	color_printk(WHITE,BLACK,"Hello\t\t World!\n");
+	for(i = 0 ;i<1440*20;i++)
+	{
+		*((char *)addr+0)=(char)0x00;
+		*((char *)addr+1)=(char)0x00;
+		*((char *)addr+2)=(char)0xff;
+		*((char *)addr+3)=(char)0x00;	
+		addr +=1;	
+	}
+	for(i = 0 ;i<1440*20;i++)
+	{
+		*((char *)addr+0)=(char)0x00;
+		*((char *)addr+1)=(char)0xff;
+		*((char *)addr+2)=(char)0x00;
+		*((char *)addr+3)=(char)0x00;	
+		addr +=1;	
+	}
+	for(i = 0 ;i<1440*20;i++)
+	{
+		*((char *)addr+0)=(char)0xff;
+		*((char *)addr+1)=(char)0x00;
+		*((char *)addr+2)=(char)0x00;
+		*((char *)addr+3)=(char)0x00;	
+		addr +=1;	
+	}
+	for(i = 0 ;i<1440*20;i++)
+	{
+		*((char *)addr+0)=(char)0xff;
+		*((char *)addr+1)=(char)0xff;
+		*((char *)addr+2)=(char)0xff;
+		*((char *)addr+3)=(char)0x00;	
+		addr +=1;	
+	}
+
+	color_printk(YELLOW,BLACK,"Hello\t\t World!\n");
+
+	// load_TR(8);
+
+	// set_tss64(0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00);
+
+	// sys_vector_init();
+
+	i = 1/0;
+//	i = *(int *)0xffff80000aa00000;
 
 	while(1)
 		;
