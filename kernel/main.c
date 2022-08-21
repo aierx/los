8ba290db43abb0fcf1221af5887019c46856eb7d
leiwenyong <aleiwenyong@outlook.com>
2022-08-22 00:13:24 +0800

    memory

--- kernel/main.c
+++ kernel/main.c
@@ -6,7 +6,7 @@
 void Start_Kernel(void)
 {
 	int *addr = (int *)0xffff800000a00000;
-
+	int i;
 
 	Pos.XResolution = 1440;
 	Pos.YResolution = 900;
@@ -20,27 +20,14 @@ void Start_Kernel(void)
 	Pos.FB_addr = (int *)0xffff800000a00000;
 	Pos.FB_length = (Pos.XResolution * Pos.YResolution * 4);
 
-	color_printk(YELLOW,BLACK,"Hello World!\n");
-
 	load_TR(8);
 
-	set_tss64(
-		0xffff800000007c00, 
-		0xffff800000007c00, 
-		0xffff800000007c00, 
-		0xffff800000007c00, 
-		0xffff800000007c00, 
-		0xffff800000007c00, 
-		0xffff800000007c00, 
-		0xffff800000007c00, 
-		0xffff800000007c00, 
-		0xffff800000007c00);
+	set_tss64(0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00);
 
 	sys_vector_init();
 
-	int i;
-	// i = 1/0;
-	// i = *(int *)0xffff80000aa00000;
+	color_printk(RED,BLACK,"memory init \n");
+	init_memory();
 
 	while(1)
 		;
