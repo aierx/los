9e733be8c52435c24f8032867787504ae4041617
leiwenyong <aleiwenyong@outlook.com>
2022-08-21 20:33:13 +0800

    添加中断和异常处理

--- kernel/main.c
+++ kernel/main.c
@@ -55,14 +55,14 @@ void Start_Kernel(void)
 
 	color_printk(YELLOW,BLACK,"Hello\t\t World!\n");
 
-	// load_TR(8);
+	load_TR(8);
 
-	// set_tss64(0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00);
+	set_tss64(0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00, 0xffff800000007c00);
 
-	// sys_vector_init();
+	sys_vector_init();
 
-	i = 1/0;
-//	i = *(int *)0xffff80000aa00000;
+	// i = 1/0;
+	i = *(int *)0xffff80000aa00000;
 
 	while(1)
 		;
