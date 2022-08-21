b594eb0752f1596ea7b994963f91e191ce85c20b
leiwenyong <aleiwenyong@outlook.com>
2022-08-21 10:55:47 +0800

    markdown

--- kernel/main.c
+++ kernel/main.c
@@ -1,4 +1,60 @@
 void Start_Kernel(void){
+	int *addr = (int *)0xffff800000a00000;
+	int i;
+
+
+	for(i = 0 ;i<1440*20;i++)
+	{
+        int j = 1000;
+        while (--j>0)
+        {
+        }
+		*((char *)addr+0)=(char)0x00;
+		*((char *)addr+1)=(char)0x00;
+		*((char *)addr+2)=(char)0xff;
+		*((char *)addr+3)=(char)0x00;	
+		addr +=1;	
+	}
+	for(i = 0 ;i<1440*20;i++)
+	{
+        int j = 1000;
+        while (--j>0)
+        {
+        }
+		*((char *)addr+0)=(char)0x00;
+		*((char *)addr+1)=(char)0xff;
+		*((char *)addr+2)=(char)0x00;
+		*((char *)addr+3)=(char)0x00;	
+		addr +=1;	
+	}
+	for(i = 0 ;i<1440*20;i++)
+	{
+        int j = 1000;
+        while (--j>0)
+        {
+        }
+		*((char *)addr+0)=(char)0xff;
+		*((char *)addr+1)=(char)0x00;
+		*((char *)addr+2)=(char)0x00;
+		*((char *)addr+3)=(char)0x00;	
+		addr +=1;	
+	}
+	for(i = 0 ;i<1440*20;i++)
+	{
+        int j = 1000;
+        while (--j>0)
+        {
+        }
+        
+		*((char *)addr+0)=(char)0xff;
+		*((char *)addr+1)=(char)0xff;
+		*((char *)addr+2)=(char)0xff;
+		*((char *)addr+3)=(char)0x00;	
+		addr +=1;	
+	}
+
+    
+
     while (1)
         ;
 }
--- readme.md
+++ readme.md
@@ -1,14 +1,50 @@
 hello leiwenyong
 
-打印内存
-
+```bash
+# 打印内存
 xp /32bx 0x7c00
 
-反汇编
-
+# 反汇编
 objdump -D system
+```
 
-- real mode
-- protect mode
-- compatibility mode
-- long mode
+### CPU工作模式
+
+- 实模式 real mode
+- 保护模式 protect mode
+- 兼容模式 compatibility mode
+- 平坦模式 long mode
+
+### 寻址方式
+- 立即数：
+  - mov ax,1234h
+  - mov [bx],5678h
+- 直接：[0x1111h] 或 symbol
+  - mov ax,es:[1234h]
+  - mov dx,VALUE
+  - mov dx,[VALUE]
+- 间接：[bx]
+  - mov ax，[bx]
+- 间接相对：[bx]+0x1111h
+  - mov ax,[bx]+12h
+  - mov ax,[si]+5678h
+  - mov ax,[bp]+1234h
+- 基址变址：
+  - mov ax,[bx+si]
+  - mov ax,[bp+di]
+- 基址变址相对：
+  - mov ax,[bx+si]+12h
+  - mov ax,[bp+di]+1234h
+- 段内直接转移
+    - 段内直接短转移：cs不变，ip+(-127 ~ 127)
+      - jmp near ptr NEAR_NEW_ADDR
+    - 段内直接近转移：cs不变，ip+(-32767 ~ 32767)
+      - jmp bx
+      - jmp word ptr [bx]+1234h
+    - 段间直接寻址：cs和ip同时改变
+      - jmp 1234h：5678h
+      - jmp far ptr NEW_ADDR
+    - 段间间接寻址：
+        - jmp dword ptr [bx][si]+1234h
+        - jmp dword ptr [1234h]
+        - jmp dword ptr [si]
